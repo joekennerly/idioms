a1eb2334028972da13d18d5c1f3df6dc87a3eaec
joekennerly <joseph.kennerly@gmail.com>
2019-08-26 14:56:16 -0500

    init

---
+++ idioms.py
@@ -0,0 +1,17 @@
+idioms = {
+    "Penny": ["A", "penny", "for", "your", "thoughts"],
+    "Injury": ["Add", "insult", "to", "injury"],
+    "Moon": ["Once", "in", "a", "blue", "moon"],
+    "Grape": ["I", "heard", "it", "through", "the", "grapevine"],
+    "Murder": ["Kill", "two", "birds", "with", "one", "stone"],
+    "Limbs": ["It", "costs", "an", "arm", "and", "a", "leg"],
+    "Grain": ["Take", "what", "someone", "says", "with", "a", "grain", "of", "salt"],
+    "Fences": ["I'm", "on", "the", "fence", "about", "it"],
+    "Sheep": ["Pulled", "the", "wool", "over", "his", "eyes"],
+    "Lucifer": ["Speak", "of", "the", "devil"],
+}
+str = " "
+
+for key, value in idioms.items():
+    my_string = f"{key}: {str.join(value)}"
+    print(my_string)
